51177ac8c10d25d8ad97c0be9a1e22b5d9b443cf
Mark Liang <mark@vdr.tw>
2025-09-18 15:08:21 +0800

    feat: add edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-// import { useState } from "react";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
@@ -6,12 +7,56 @@ import FilterButton from "./components/FilterButton";
 function App(props: {
   tasks: Array<{ id: string; name: string; completed?: boolean }>;
 }) {
-  const taskList = props.tasks?.map((task) => (
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name: string) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  const tasksNoun = tasks.length !== 1 ? "tasks" : "task";
+  const headingText = `${tasks.length} ${tasksNoun} remaining`;
+
+  function toggleTaskCompleted(id: string) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id: string) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id: string, newName: string) {
+    const editedTaskList = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // Copy the task and update its name
+        return { ...task, name: newName };
+      }
+      // Return the original task if it's not the edited task
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  const taskList = tasks?.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
 
@@ -19,13 +64,13 @@ function App(props: {
     <>
       <div className="todoapp stack-large">
         <h1>TodoMatic</h1>
-        <Form></Form>
+        <Form onAdd={addTask}></Form>
         <div className="filters btn-group stack-exception">
           <FilterButton></FilterButton>
           <FilterButton></FilterButton>
           <FilterButton></FilterButton>
         </div>
-        <h2 id="list-heading">3 tasks remaining</h2>
+        <h2 id="list-heading">{headingText}</h2>
         <ul
           role="list"
           className="todo-list stack-large stack-exception"
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,29 @@
-function Form() {
+import { useState } from "react";
+
+function Form(props: { onAdd: (name: string) => void }) {
+  const [name, setName] = useState("");
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    props.onAdd(name);
+    setName("");
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
           What needs to be done?
         </label>
       </h2>
       <input
+        value={name}
+        onChange={(ev) => setName(ev.target.value)}
         type="text"
         id="new-todo-input"
         className="input input__lg"
         name="text"
         autoComplete="off"
+        required
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
